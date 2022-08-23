5768763c7299e8b9bd85ed31a2622fdb11f3d08e
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-08-23 15:23:34 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -45,4 +45,6 @@ drawFilledRect(-10, height - 600, 1400, 400, 'lightblue');
 
 drawFilledCircle(width - 475, height - 575, 50, 'yellow');
 
+drawFilledRect(165, height - 375, 175, 225, 'darkbrown');
+
 drawFilledRect(165, height - 375, 175, 225, 'brown');
