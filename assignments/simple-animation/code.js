647038722064ce4990070122db1680f2c380670d
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-08-25 15:15:31 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -6,15 +6,14 @@
 // number of milliseconds (one millionth of a second) since the program started.
 const drawFrame = (time) => {
   clear();
-  drawFilledCircle((time / 10) % width, height / 1.6, 17, 'black');
-  
-  drawFallingRect(width / 2, time);
+  drawFilledCircle((time / 3) % width, height / 2, 25, 'blue');
+  drawFallingTriangle(width / 2, time);
 };
 
 // This is a function that we define to make it easier to draw a triangle. You
 // may want to experiment with writing your own functions using this one as a
 // model to draw other shapes.
-const drawRect = (x1, y1, x2, y2, x3, y3, color, width = 1) => {
+const drawTriangle = (x1, y1, x2, y2, x3, y3, color, width = 1) => {
   drawLine(x1, y1, x2, y2, color, width);
   drawLine(x2, y2, x3, y3, color, width);
   drawLine(x3, y3, x1, y1, color, width);
@@ -22,24 +21,22 @@ const drawRect = (x1, y1, x2, y2, x3, y3, color, width = 1) => {
 
 // This draws a falling trangle of a particular shape whose bottom point is
 // positioned at x and whose y is a function of time.
-const drawFallingRect = (x, time) => {
+const drawFallingTriangle = (x, time) => {
   // Figure out the x values relative to the passed in x
   let x1 = x - 50;
   let x2 = x;
   let x3 = x + 50;
 
   // Figure out the y values as a function of time.
-  let y1 = (time / 3) % height;
+  let y1 = (time / 4) % height;
   let y2 = y1 + 37;
   let y3 = y1 - 13;
 
   // Actually draw the triangle.
-  drawTriangle(x1, y1, x2, y2, x3, y3, 'pink', 1);
+  drawTriangle(x1, y1, x2, y2, x3, y3, 'pink', 3);
 };
 
 // Leave this code here or the animation won't run. Also don't change the name
 // of drawFrame either here or where it is defined. (Or, if you must, change it
 // the same way in both places.)
 animate(drawFrame);
-
-  drawFilledRectangle((time / 10) % width, height / 1.6, 17, 'black');
