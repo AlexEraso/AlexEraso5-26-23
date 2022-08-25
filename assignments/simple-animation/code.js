100fd6356faf25914b47becc236ebc6891dedc35
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-08-25 14:56:53 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -41,3 +41,58 @@ const drawFallingTriangle = (x, time) => {
 // the same way in both places.)
 animate(drawFrame);
 
+/*
+ * This code is running in an environment with five functions defined:
+ *
+ *  drawLine(x1, y1, x2, y2, color, lineWidth)
+ *
+ *    Draws a line from x1,y1 to x2,y2 using the give color. The fifth argument,
+ *    lineWidth, is optional and defaults to 1.
+ *
+ *  drawCircle(x, y, r, color, lineWidth=1)
+ *
+ *    Draws a circle centered at x,y with radius r using the given color. The
+ *    fith argument, lineWidth, is optional and defaults to 1.
+ *
+ *  drawRect(x, y, width, height, color, lineWidth=1)
+ *
+ *    Draws a rectangle starting at x,y with the given width, height, and color.
+ *    Positive widths go to the right and negative to the left; positive heights
+ *    go down and negative heights go up. The sixth argument, lineWidth, is
+ *    optional and defaults to 1.
+ *
+ *  drawFilledRect(x, y, width, height, color)
+ *
+ *    Draws a filled rectangle starting at x,y with the given width, height, and
+ *    color. Positive widths go to the right and negative to the left; positive
+ *    heights go down and negative heights go up.
+ *
+ *  drawFilledCircle(x, y, r, color)
+ *
+ *    Draws a filled circle centered at x,y with radius r using the given color.
+ *
+ * There are also a couple variables that might be useful:
+ *
+ *  width - the width of the drawing area.
+ *  height - the height of the drawing area.
+ *
+ */
+
+
+
+
+
+drawFilledRect(-10, height - 200, 900, 300, 'green');
+
+drawFilledRect(-10, height - 600, 1400, 400, 'lightblue');
+
+drawFilledCircle(width - 475, height - 575, 50, 'yellow');
+
+
+
+drawFilledRect(110, height - 375, 300, 225, 'grey');
+
+drawFilledRect(225, height - 260, 75, 115, 'brown');
+
+drawFilledCircle(width - 210, height - 190, 5, 'white');
+
