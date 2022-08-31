39d8ff8a7a278c9ccd403d3985acefc3a2c67bd7
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-08-31 15:26:00 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -8,35 +8,8 @@ const drawFrame = (time) => {
   clear();
 
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
+  
   drawFilledRect(-10, height - 200, 900, 300, 'green');
 
 drawFilledRect(-10, height - 600, 1400, 400, 'lightblue');
@@ -51,7 +24,7 @@ drawFilledRect(225, height - 260, 75, 115, 'brown');
 
 drawFilledCircle(width - 210, height - 190, 5, 'white');
   drawFilledCircle((time / 1.9) % width, height / 1.2, 15, 'white');
-  drawFilledCircle((time / 2) % width, height / 1.2, 15, 'blue');
+  drawFilledCircle((time / 2) % width, height / 1.2, 15, 'darkblue');
     drawFilledCircle((time / 2.1) % width, height / 1.2, 15, 'red');
 
 
