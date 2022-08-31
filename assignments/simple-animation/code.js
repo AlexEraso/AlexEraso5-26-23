b219aa4e6a336a4e3789ebed3498ed9de9bb921f
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-08-31 15:17:52 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -14,14 +14,14 @@ drawFilledCircle(width - 475, height - 575, 50, 'yellow');
 
 
 
-drawFilledRect(110, height - 335, 300, 225, 'grey');
+drawFilledRect(110, height - 375, 300, 225, 'grey');
 
-drawFilledRect(225, height - 220, 75, 115, 'brown');
+drawFilledRect(225, height - 260, 75, 115, 'brown');
 
 drawFilledCircle(width - 210, height - 190, 5, 'white');
   drawFilledCircle((time / 1.9) % width, height / 1.2, 15, 'white');
   drawFilledCircle((time / 2) % width, height / 1.2, 15, 'blue');
-    drawFilledCircle((time / 2.1) % width, height / 1.1, 15, 'red');
+    drawFilledCircle((time / 2.1) % width, height / 1.2, 15, 'red');
 
 
   drawFallingFilledTriangle(width / 2, time);
