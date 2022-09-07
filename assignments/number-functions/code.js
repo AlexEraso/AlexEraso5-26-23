b3b2fa7007c4fecacd25841fe69c46f9a740c777
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-07 15:32:29 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -8,3 +8,10 @@ const divide = (x,y) => x / y;
 
 const mod = (x,y) => x % y;
 
+const averageOfTwo  = (x,y) => (x + y)/ 2;
+
+const averageOfThree = (x,y,z) (x + y + z)/3;
+
+const distance  = (x,y) Math.abs(x-y)	
+
+const manhattanDistance = (x1,x2,y1,y2) Math.abs(x1 - x2) + Math.abs(y1 - y2)	
