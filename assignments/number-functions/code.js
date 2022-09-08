4d23c02f8f3f8855bc9e56fbd4c10c44b06a4a19
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-08 14:52:27 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -12,4 +12,14 @@ const averageOfTwo  = (x,y) => (x + y)/ 2;
 
 const averageOfThree = (x,y,z) => (x + y + z)/3;
 
+const distance  = (x,y) Math.abs(x-y)	
+
+let a = x1 - x2;
+let b = y1 - y2;
+
+var c = Math.sqrt( a*a + b*b );
+
+const manhattanDistance = (x1,x2,y1,y2) Math.abs(x1 - x2) + Math.abs(y1 - y2)	
+
+const euclideanDistance =
 
