966267c8c8e576f383e5f6368130e21e09bfad82
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-09 14:41:12 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -19,9 +19,9 @@
 
 const isFlobbyBird = (red , spotted) => red && spotted ;
 
-const isBloggyBird = (red , spotted) => red || spotted;
+const isBloggyBird = (red , spotted) => red && spotted;
 
-const isFlibbleBird =
+const isFlibbleBird = ()
 
 const isGlobbyBird =
 
