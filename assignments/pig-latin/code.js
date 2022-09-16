f5ffa81971d29c1c5e03b8693ed774931a7bb106
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-16 15:17:15 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -39,10 +39,8 @@
 
 
 const pigLatin = (word) =>
-word.substring(word.search(/[aeiou]/)) + 
-word.substring (0, word.search(/[aeiou]/)) + 'ay'
+  word.substring(word.search(/[aeiou]/)) + word.substring(0, word.search(/[aeiou]/)) + 'ay'
 
 const advancedPigLatin = (word) =>
-word.search(/[aeiou]/) === 0 ? word + 'way' :
-word.substring(word.search(/[aeiou]/)) + 
-word.substring (0, word.search(/[aeiou]/)) + 'ay'
+  word.search(/[aeiou]/) === 0 ? word + 'way' :
+    word.substring(word.search(/[aeiou]/)) + word.substring(0, word.search(/[aeiou]/)) + 'ay'
