ccbf79fc9b58e63b80715e73b49e26a2731cff32
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-20 15:12:49 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,7 +1,4 @@
 
-const drawFilledCircle = 
-
-for (let x = 0; x < 100; x++ )
-
-drawFilledCircle width - 100, height - 250, 50, 'blue');
+for (let x = 0; x < 100; x++) {
+  drawFilledCircle(width - 100, height - 250, 50, 'blue');
 }
