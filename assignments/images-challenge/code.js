9b6a48feb178b780c241d4a0ff192b8b2c84dd1a
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-20 14:54:09 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,1 +1,9 @@
+const x =drawFilledCircle(width - 100, height - 250, 50, 'blue');
+
 drawFilledCircle(width - 100, height - 250, 50, 'blue');
+
+let x = 0;
+while (x < 100) {
+  console.log(x);
+  x++;
+}
