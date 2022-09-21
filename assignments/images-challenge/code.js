d2e3822465436bc2e269184cf2598c9d7b2582c9
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-21 15:26:09 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,9 +1,1 @@
-for (let x = 0; x < 100; x++) {
- if (x % 2 ==0){
- drawFilledCircle(x* 25, height - 250, 13, 'blue');
-} else{
-drawFilledCircle(x*25, height - 250, 13, 'red');
-}
-}
-
-
+drawLine(x1, y1, x2, y2, color, 15)
