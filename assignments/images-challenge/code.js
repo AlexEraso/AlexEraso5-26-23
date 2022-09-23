17bd2a6517c242f43de71b18bcf3190536efe979
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-23 14:37:45 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,5 +1,6 @@
 
 //Line of Circles
+
 const lineofcircles = (r) => {
   for (let x = 0; x < width/(r*2); x++) {
     drawFilledCircle(x * r*2, height - 250, r, 'blue');
@@ -9,7 +10,7 @@ const lineofcircles = (r) => {
 lineofcircles(13)
 
 //Line of Circles Alternating 
-/*
+
 for (let x = 0; x < 100; x++) {
   if (x % 2 == 0) {
     drawFilledCircle(x * 25, height - 250, 13, 'blue');
@@ -21,4 +22,3 @@ for (let x = 0; x < 100; x++) {
 //Concentric Circles
 
 drawFilledCircle(width - 475, height - 575, 50, 'yellow');
-*/
