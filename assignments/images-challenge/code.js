8fb491e0660d51169594df41c3445a217a2244ad
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-23 14:38:03 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,6 +1,6 @@
 
 //Line of Circles
-
+/*
 const lineofcircles = (r) => {
   for (let x = 0; x < width/(r*2); x++) {
     drawFilledCircle(x * r*2, height - 250, r, 'blue');
@@ -8,7 +8,7 @@ const lineofcircles = (r) => {
 };
 
 lineofcircles(13)
-
+*/
 //Line of Circles Alternating 
 
 for (let x = 0; x < 100; x++) {
