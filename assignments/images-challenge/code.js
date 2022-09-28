f61dc4005a64058560b64af37492d54133e89d3a
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-28 15:21:32 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -23,7 +23,8 @@ for (let x = 0; x < 100; x++) {
 
 const cocentriccircles = (n) => {
   for (let x = 0; x < width / n * 10; x++) {
-  if drawFilledCircle(width - 450, height - 230, 225, 'yellow');
+  if (x % 2 ==0)
+  drawFilledCircle(width - 450, height - 230, 225, 'yellow');
   } else }
     drawFilledCircle(width - 450, height - 230, 225-x*10, 'black');
   }
