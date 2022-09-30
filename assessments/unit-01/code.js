9cac9f4283cda2a359d8b27b358755e561e2b6dc
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-09-30 15:19:49 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -5,3 +5,33 @@ const emit = (a, b, product) => {
 };
 
 // Write your code here ...
+
+const averageWeight = (x , y) => (x + y)/2;
+
+const hypotenuse = (a , b) => (a ** 2 ) + (b **2);
+
+const maxRadius = (width , height) => 
+
+const numCircles = (radius , width) => 
+
+const offset = (width , ) => 
+
+const canSleepIn = (weekday , vacation) => weekday && vacation || !weekday && vacation; 
+
+const canGoToProm = (senior , invited , exclusion) => senior && !exclusion || !senior && invited; 
+
+const getsSpeedingTicket = (speed , grouchy) => speed && !grouchy || !speed && grouchy;  
+
+const moreThanTwiceAsLong = () => 
+
+const aFartherThanB = () => 
+
+const firstHalf = () => 
+
+const secondHalf = () => 
+
+const upDown = () =>
+
+const everyOther = () => 
+
+const 
