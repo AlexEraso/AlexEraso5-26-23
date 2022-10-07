9d0f030756f3d788e89e2df3fad62da209b360df
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-07 15:02:58 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -7,3 +7,43 @@ const G = 6.6743e-11;
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+const itemsLeftOver = (people , items) => {
+  return items%people
+};
+
+const areaOfCircle = (radius) => {
+  return Math.PI*radius**2;
+};
+
+const volumeOfCube = (sideLength) => {
+  return sideLength**3
+};
+
+const populationGrowth = (currentPopulation , growth) => {
+  return currentPopulation  
+};
+
+const earnedRunAverage = (earnedRuns , innings) => {
+  return (earnedRuns / innings) * 9;
+};
+
+const valueOfJewels = (diamonds , emeralds , diamondInGold , emeraldInGold) => {
+  return ;
+};
+
+const payWithOvertime = (hoursWorked , hourlyRate , overtimeRate) => {
+  return (hoursWorked * hourlyRate) + ((hoursWorked - 8) * overtimeRate) ;
+};
+
+const firstClassPostage = (weightOfLetter) => {
+  return ;
+};
+
+const weightOnJupiter = (weightOfPerson) => {
+  return weightOfPerson EARTH;
+};
+
+const gravity = () => {
+  return;
+};
+
