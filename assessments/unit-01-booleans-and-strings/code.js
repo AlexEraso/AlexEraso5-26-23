79213a121410865c8511f081c902b2dafd0e2bfe
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-17 15:13:04 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -9,3 +9,35 @@ const rand = (n) => Math.floor(Math.random() * n);
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+const fireAlarm = (pulled , smoke , drill) => {
+  return pulled || smoke || drill
+};
+
+const canBePresident = ( thirtyFive, citizen , fourteenYears) => {
+  return thirtyFive || citizen || fourteenYears
+};
+
+const willSeeTweet = (follows , retweeted, blocked) => {
+  return follows || retweeted || blocked
+};
+
+const evenGreaterThanZero = (number) => {
+  return even && number<0
+};
+
+const isLeapYear = () => {
+  return 
+};
+
+const firstAndLast = (s) => {
+  return s.substring(1)	+ s.substring(s.length - 1, s.length)	
+};
+
+const swapFrontAndBack = (s) => {
+  return
+};
+
+const simplePigLatin = () => {
+  return 
+};
