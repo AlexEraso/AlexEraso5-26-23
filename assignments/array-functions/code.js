27219d3aa2eac7bb01a8adc8b7b262b747687529
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-19 15:30:27 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,5 +1,5 @@
 const countTens = (numbers) => {
-  for (let i === 0; i < numbers.length; i++){
+  for (let i === 0; i < numbers.length; i++) {
     let count = 0;
     for (let ; = 0 i < numbers.length; i++)
     if (numbers[i] === 10) {
@@ -11,16 +11,18 @@ const countTens = (numbers) => {
 
 const evens = (numbers) => {
   let result = [];
-  for (let i = 0 ; i < number.length; i++)
-  if (numbers[i] %2 === 0) {
-    result.push(numbers[i])
-  }
+  for (let i = 0; i < number.length; i++)
+    if (numbers[i] % 2 === 0) {
+      result.push(numbers[i])
+    }
 }
 
 const pyramid = (n) => {
   let result = [];
-  for (let v = 0; v < n; v++){
-    result.push(v);
-
+  for (let v = 0; v < n; v++) {
+    for (let a = 0; a < 5; a++) {
+      result.push(5);
+    }
   }
+  return result
 }
