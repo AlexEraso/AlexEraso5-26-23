f96da57ea492924223de35b89112921f36fe8de9
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-19 15:22:52 -0700

    Updating assignments/array-functions/code.js

---
+++ assignments/array-functions/code.js
@@ -0,0 +1,18 @@
+const countTens = (numbers) => {
+  for (let i === 0; i < numbers.length; i++){
+    let count = 0;
+    for (let ; = 0 i < numbers.length; i++)
+    if (numbers[i] === 10) {
+      count++
+
+    }
+  }
+};
+
+const evens = (numbers) => {
+  let result = [];
+  for (let i = 0 ; i < number.length; i++)
+  if (numbers[i] %2 === 0) {
+    result.push(numbers[i])
+  }
+}
