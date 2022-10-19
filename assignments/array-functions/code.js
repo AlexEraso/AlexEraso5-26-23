dfb8fcbe4a7f68550711686d009f01f7eaf1a6f1
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-19 15:32:21 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -20,7 +20,7 @@ const evens = (numbers) => {
 const pyramid = (n) => {
   let result = [];
   for (let v = 0; v < n; v++) {
-    for (let a = 0; a < 10; a++) {
+    for (let a = 0; a < 2; a++) {
       result.push(42);
     }
   }
