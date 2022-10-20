a947a23fda15ff6a9ceb747ef702c3b7ca99e08c
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-20 15:23:51 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -3,9 +3,9 @@ const countTens = (numbers) => {
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] === 10) {
       tens = tens + 1
-      return tens
+    
     }
   }
+ return tens 
 };
-
 
