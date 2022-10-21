26665c5664247be70e2bd8028bfa1b015cc82594
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-20 19:12:35 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -35,6 +35,17 @@ const anyOverOneHundred = (numbers) => {
   return false
 }
 
+const 
+
+//So, I didn't realize how late it was getting. We are just now eating, and I have to do Coop's bedtime afterward. I won't be free for another hour at least. So, here's what I offer you for the pyramid instead:
+
+//* Build a return array
+//* Starting at 0 and counting up to the provided number (function parameter)...
+//Get the current number, and create another loop. Start at 0 again and count up to whatever number you're on.
+//On each iteration of the loop, append whatever number you're on to the return array
+//After the second loop finishes, you will have the appropriate number of digits (one 1s, two 2s, etc.), and the first loop will keep going until the number provided to the function. Then you return your array and bada-bing, bada-boom!
+//The point of the question is to do a nested for-loop
+
 
 
 
