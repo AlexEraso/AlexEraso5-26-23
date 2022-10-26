acfb27339602c4cbefac18f8cc917426f8f4c1a0
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-26 15:21:49 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -9,7 +9,15 @@ registerOnclick((x, y) => {
   drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
 });
 const board = () => {
-for (let i = 0; i < 2; i++)
+for (let i = 0; i < 2; i++) {
 const x = (i + 1) * width / 3
+drawLine(x, 0, x, height, 'black', 5);
+}
+for (let i = 0; i < 2; i++) {
+  const x = (i + 1) * height / 3 
 drawLine(0, x, width, x, 'black', 5);
 }
+}
+
+board()
+
