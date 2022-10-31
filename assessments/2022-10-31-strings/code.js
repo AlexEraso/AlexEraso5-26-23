13087850cdedc94e771fe79fbf63f76724e3aafd
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-10-31 15:08:24 -0700

    Updating assessments/2022-10-31-strings/code.js

--- assessments/2022-10-31-strings/code.js
+++ assessments/2022-10-31-strings/code.js
@@ -39,4 +39,22 @@ const swapFrontAndBack = (s) => {
   return s.substring (s.length/2 ) + s.substring(0, s.length/2)
 };
 
+const simplePigLatin = (s, firstVowel) => {
+  return s.substring(firstVowel) + s.substring(0, firstVowel) + 'ay';
+};
 
+const randomCharacter = (s) => {
+  return s[rand(s.length)];
+};
+
+const randomCharacterUpDown = (s) => {
+  const i = rand(s.length);
+  return s[i].toUpperCase() + s[i].toLowerCase();
+};
+
+const isAllUpperCase = (s) => {
+  if (s === s.toUpperCase(s)){
+  return true;
+  }else{
+  return false;
+}};
