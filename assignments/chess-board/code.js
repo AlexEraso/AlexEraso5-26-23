2015ab61d880f696fd9d531e7b8967bc7fc48d71
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-03 15:18:09 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -17,7 +17,7 @@ drawText(WHITE_QUEEN, width/2.4, height/8, 'black', 64);
 drawText(WHITE_BISHOP, width/3.4, height/8, 'black', 64);
 drawText(WHITE_BISHOP, width/1.5, height/8, 'black', 64);
 drawText(WHITE_KNIGHT, width/1.27, height/8, 'black', 64);
-drawText(WHITE_KNIGHT, width/5, height/8, 'black', 64);
+drawText(WHITE_KNIGHT, width/5.9, height/8, 'black', 64);
 
 
 
