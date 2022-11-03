a24b3921a20a430a5dccf69fce7b17f379cd3b3b
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-03 15:13:00 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -14,7 +14,7 @@ const BLACK_PAWN   = '♟';
 // Example of drawing one of the pieces
 drawText(WHITE_KING, width/1.85, height/8, 'black', 64);
 drawText(WHITE_QUEEN, width/2.4, height/8, 'black', 64);
-drawText(WHITE_BISHOP, width/3, hieght/8, 'black', 64);
+
 
 
 const board = () => {
