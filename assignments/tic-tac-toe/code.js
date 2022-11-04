c505fefc8becbe530815756a77559ed2ebccc7db
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-04 15:03:33 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,6 +5,34 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 
+let move = 0;
+
 registerOnclick((x, y) => {
-  drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
+  if (move % 2 === 0) {
+    drawText('X', x, y, 'red', Math.min(width, height) * 0.3);
+  } else {
+    drawText('O', x, y, 'blue', Math.min(width, height) * 0.3);
+  }
+  move++;
 });
+const board = () => {
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * width / 3
+    drawLine(x, 0, x, height, 'black', 5);
+  }
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * height / 3
+    drawLine(0, x, width, x, 'black', 5);
+  }
+
+}
+
+board()
+
+
+
+//if movenumber %2===0 make it x else make it o 
+
+//how do i make the move number increase every click, let move = 0 then like each click ++ 
+
+
