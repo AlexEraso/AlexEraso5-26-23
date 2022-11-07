d33b5f68f578c27e6b32ba127d3aadeace54d4e2
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-07 15:06:47 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -3,8 +3,6 @@ const getX = (x) => {
 }
 
 const point = (x , y) => {
-  return { 'x' : 10, 'y' : 20}
-  return { 'x' : 20, 'y' : 30}
-  return { 'x' : 0, 'y' : 0}
+  return { 'x' : 10, 'y' : 20} { 'x' : 20, 'y' : 30} { 'x' : 0, 'y' : 0}
 
 }
