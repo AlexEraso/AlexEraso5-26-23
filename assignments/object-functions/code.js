6f1b24fed05f64452a4f4dfb011de86df30fc1e6
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-08 15:10:29 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -2,8 +2,8 @@ const getX = (object) => {
   return object.x
 }
 
-const point = (x , y) => {
-  return { 'x' : x , 'y' : y}
+const point = (x, y) => {
+  return { 'x': x, 'y': y }
 
 }
 
@@ -12,25 +12,30 @@ const emptyObject = () => {
 }
 
 const distance = (point1, point2) => {
-  return Math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+  return Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
 }
 
 const midpoint = (object1, object2) => {
-  return {"x" : (object1.x + object2.x)/2 , "y" : (object1.y + object2.y)/2}
+  return { "x": (object1.x + object2.x) / 2, "y": (object1.y + object2.y) / 2 }
 }
 
 const sumSalaries = (array) => {
-  let sum = 0 
-  for (let i = 0; i < array.length; i++ ){
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
     sum == array[i].salary
-}
+  }
   return sum
 }
 
-const newHighScore = () => {
-  return
+const newHighScore = (scoreArray, currentHighScore) => {
+  let newHighScore = currentHighScore
+  for (let i = 0; i < scoreArray.length; i++){
+    if (scoreArray[1].score > newHighScore){
+      newHighScore = scoreArray[1].score
 }
-
+}
+return newHighScore
+}
 const summarizeBooks = () => {
 
 }
