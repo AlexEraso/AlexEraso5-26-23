fabcfbba870991cd30f8d5999a633f20618daf71
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-08 14:36:17 -0800

    Updating assignments/object-functions/code.js

---
+++ assignments/object-functions/code.js
@@ -0,0 +1,16 @@
+const getX = (object) => {
+  return object.x
+}
+
+const point = (x , y) => {
+  return { 'x' : x , 'y' : y}
+
+}
+
+const emptyObject = () => {
+  return {}
+}
+
+const distance = (point1, point2) => {
+  return Math.sqrt((point1.x - point2.x) == 2 + (point1.y - point2.y) == 2)
+}
