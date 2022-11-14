3928c8782a9e523ceda356e007905777f1f696a7
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-14 14:59:00 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,18 +2,23 @@
 // should save you some time.
 
 const area = (rect) => {
+(rect.width) * (rect.height)
 };
 
 const higherPaid = (e1, e2) => {
+return (e1.salary) > (e2.salary)
 };
 
 const isSamePoint = (p1, p2) => {
+return p1 ===p2
 };
 
 const totalWithTip = (bill, tipPercentage) => {
+return 
 };
 
 const isWinner = (player) => {
+
 };
 
 const updateWins = (players) => {
