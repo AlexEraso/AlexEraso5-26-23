d12fded17f7847582ba56d0b97f0dc255bc5f3f3
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-21 20:49:24 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -9,7 +9,7 @@ let move = 0;
 
 registerOnclick((x, y) => {
   if (move % 2 === 0) {
-    drawText('X', 0.001, y, 'red', Math.min(width, height) * 0.3);
+    drawText('X', 0.0000001, y, 'red', Math.min(width, height) * 0.3);
   } else {
     drawText('O', x, y, 'blue', Math.min(width, height) * 0.3);
   }
