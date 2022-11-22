d701f9832cb8b366b93666a06a6f1a2fca267b8a
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-21 21:08:56 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,15 +1,8 @@
-// This is a bit of a new thing. registerOnclick is a function provided by the
-// framework. But the argument we're passing to it is *another* function. Notice
-// how the argument here looks like what we normally put on the righthand side
-// of the equals sign in our normal `const foo = ...` function definition. This
-// is called an anonymous function. We'll discuss this in more detail in a few
-// weeks but for now you can just adapt this code.
-
 let move = 0;
 
 registerOnclick((x, y) => {
-  if (move % 2 === 0) {
-    drawText('X', x, y, 'red', Math.min(width, height) * 0.3);
+  if (move % 2 === 0) {(x < width/3 && y < height/3){
+    drawText('X', 75,  40, 'red', Math.min(width, height) * 0.3);}
   } else {
     drawText('O', x, y, 'blue', Math.min(width, height) * 0.3);
   }
@@ -28,7 +21,3 @@ const board = () => {
 }
 
 board()
-
-
-
-
