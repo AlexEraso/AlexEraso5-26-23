ce527acd919388376406c5128be59457ef4a75d3
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-24 13:09:59 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -6,11 +6,29 @@ registerOnclick((x, y) => {
 
   const row = 1 + Math.floor(y / (height / 3));
   const col = Math.floor(x / (width / 3));
+  // chk arr at row,col
+  // look for existence of any value
+  // if that position is empty, then there's no x/o
+  // so it's a safe move
+  // otherwise, skip it
 
   const xPos = (col * width / 3) + width / 9;
   const yPos = (row * height / 3) - (height / 9) + (height / 18);
 
+
   drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
+// save x to arr at row,col
+
+//  0  1  2
+//  3  4  5
+//  6  7  8
+
+// arr[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+// arr[3][3] = { {0, 1, 2},
+//               {3, 4, 5},
+//               {6, 7, 8}}
+
+
 
   move++;
 });
