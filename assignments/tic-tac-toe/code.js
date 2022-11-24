247fa1b7accd0cba86d9995db6bc2ed72c120f2a
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-23 19:26:39 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,61 +1,49 @@
-let move = 0;
+let move = 0; 
 
-registerOnclick((x, y) => {
-  if (move % 2 === 0) {
-      if (x < width/3 && y < height/3){
-        drawText('X', 75,  40, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 &&  y < height * 1/3){
-         drawText('X', 280,  40, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 &&  y < height * 1/3){
-         drawText('X', 490,  40, 'red', Math.min(width, height) * 0.3);
-      }else if(x < width/3 && y<height*2/3 && y>height*1/3){
-          drawText('X', 75,  90, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width/3 && x<width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('X', 280,  90, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('X', 490,  90, 'red', Math.min(width, height) * 0.3);
-      }else if(x < width * 1/3 && y>height*2/3){
-          drawText('X', 75,  140, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 && y>height*2/3){
-          drawText('X', 280,  140, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y>height*2/3){
-          drawText('X', 490,  140, 'red', Math.min(width, height) * 0.3);
-      }
-  }else{
-      if (x < width/3 && y < height/3){
-        drawText('O', 75,  40, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 &&  y < height * 1/3){
-         drawText('O', 280,  40, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 &&  y < height * 1/3){
-         drawText('O', 490,  40, 'blue', Math.min(width, height) * 0.3);
-      }else if(x < width/3 && y<height*2/3 && y>height*1/3){
-          drawText('O', 75,  90, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width/3 && x<width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('O', 280,  90, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('O', 490,  90, 'blue', Math.min(width, height) * 0.3);
-      }else if(x < width * 1/3 && y>height*2/3){
-          drawText('O', 75,  140, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 && y>height*2/3){
-          drawText('O', 280,  140, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y>height*2/3){
-          drawText('O', 490,  140, 'blue', Math.min(width, height) * 0.3);
-      }
-      
+registerOnclick ((x, y) => {
+  let marker;
+  let color; 
+  let xPos;
+  let yPos;
+
+  if (move % 2 === 0 ) {
+    marker = 'X';
+    color = 'red';
+  }else {
+    marker = 'O';
+    color = 'blue';
   }
-  
-  move++;
+
+  if (x < width / 3) {
+    xPos = 75;
+  } else if (x > width / 3 && x < width * 2/3){
+    xPos = 280;
+  }else if (x > width * 2/3) {
+    xPos =490;
+  }
+
+  if (y < height /3){
+    yPos = 40; 
+  } else if (y < height * 2/3 && y > height * 1/3){
+    yPos = 90;
+  }else if (y > height * 2/3){
+    yPos = 140;
+  }
+
+  drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
+
+move++;
 });
+
 const board = () => {
   for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * width / 3
-    drawLine(x, 0, x, height, 'black', 5);
+    const x = (i+1) * width /3
+    drawLine(x, 0, x, height, 'black' , 5);
   }
   for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * height / 3
-    drawLine(0, x, width, x, 'black', 5);
+    const x = (i + 1) * height / 3 
+    drawLine(0, x, width, x, 'black' , 5);
   }
-
 }
 
 board()
