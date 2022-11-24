2f623cd453d702b91aaf79be8f8ec7dea1c72f3b
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-23 19:42:05 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,48 +1,38 @@
-let move = 0; 
+let move = 0;
 
-registerOnclick ((x, y) => {
+registerOnclick((x, y) => {
   let marker;
-  let color; 
+  let color;
   let xPos;
   let yPos;
 
-  if (move % 2 === 0 ) {
+  if (move % 2 === 0) {
     marker = 'X';
     color = 'red';
-  }else {
+  } else {
     marker = 'O';
     color = 'blue';
   }
 
-  if (x < width / 3) {
-    xPos = 75;
-  } else if (x > width / 3 && x < width * 2/3){
-    xPos = 280;
-  }else if (x > width * 2/3) {
-    xPos =490;
-  }
+const col = Math.floor(x / (width /3));
+const xPos = (col * width / 3) + width / 9;
 
-  if (y < height /3){
-    yPos = 40; 
-  } else if (y < height * 2/3 && y > height * 1/3){
-    yPos = 90;
-  }else if (y > height * 2/3){
-    yPos = 140;
-  }
+const row = 1 + Math.floor(y / height 3));
+const yPos = (row * height /3) - height /9) + (height /18);
 
   drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
 
-move++;
+  move++;
 });
 
 const board = () => {
   for (let i = 0; i < 2; i++) {
-    const x = (i+1) * width /3
-    drawLine(x, 0, x, height, 'black' , 5);
+    const x = (i + 1) * width / 3
+    drawLine(x, 0, x, height, 'black', 5);
   }
   for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * height / 3 
-    drawLine(0, x, width, x, 'black' , 5);
+    const x = (i + 1) * height / 3
+    drawLine(0, x, width, x, 'black', 5);
   }
 }
 
