7c5db2af3cb1cb4eb928d0d5b973f363bc2ab3ec
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-28 15:09:30 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,35 +1,121 @@
-let move = 0;
+var board;
+var playerO = "O";
+var playerX = "X";
+var currPlayer = playerO;
+var gameOver = false;
 
-registerOnclick((x, y) => {
-  const marker = move % 2 === 0 ? 'X' : 'O';
-  const color = move % 2 === 0 ? 'red' : 'blue';
+window.onload = function() {
+    setGame();
+}
 
-  const row = 1 + Math.floor(y / (height / 3));
-  const col = Math.floor(x / (width / 3));
+function setGame() {
+    board = [
+                [' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']
+            ]
+
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            if (r == 0 || r == 1) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 0 || c == 1) {
+                tile.classList.add("vertical-line");
+            }
+            tile.innerText = "";
+            tile.addEventListener("click", setTile);
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+}
+
+function setTile() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-");    //ex) "1-2" -> ["1", "2'"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (board[r][c] != ' ') { 
+        //already taken spot
+        return;
+    }
+    
+    board[r][c] = currPlayer; //mark the board
+    this.innerText = currPlayer; //mark the board on html
+
+    //change players
+    if (currPlayer == playerO) {
+        currPlayer = playerX;
+    }
+    else {
+        currPlayer = playerO;
+    }
+
+    //check winner
+    checkWinner();
+}
 
 
-  const xPos = (col * width / 3) + width / 9;
-  const yPos = (row * height / 3) - (height / 9) + (height / 18);
+function checkWinner() {
+    //horizontally, check 3 rows
+    for (let r = 0; r < 3; r++) {
+        if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != ' ') {
+            //if we found the winning row
+            //apply the winner style to that row
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(r.toString() + "-" + i.toString());
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
 
+    //vertically, check 3 columns
+    for (let c = 0; c < 3; c++) {
+        if (board[0][c] == board[1][c] && board[1][c] ==  board[2][c] && board[0][c] != ' ') {
+            //if we found the winning col
+            //apply the winner style to that col
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(i.toString() + "-" + c.toString());                
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
 
-  drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
+    //diagonally
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        for (let i = 0; i < 3; i++) {
+            let tile = document.getElementById(i.toString() + "-" + i.toString());                
+            tile.classList.add("winner");
+        }
+        gameOver = true;
+        return;
+    }
 
-  move++;
-});
+    //anti-diagonally
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+        //0-2
+        let tile = document.getElementById("0-2");                
+        tile.classList.add("winner");
 
-const board = () => {
-  for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * width / 3
-    drawLine(x, 0, x, height, 'black', 5);
-  }90i0=jk[j[
-    i0
-    KP=
-    p=
-  ]]
-  for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * height / 3
-    drawLine(0, x, width, x, 'black', 5);
-   
+        //1-1
+        tile = document.getElementById("1-1");                
+        tile.classList.add("winner");
 
-
-   Drawline
+        //2-0
+        tile = document.getElementById("2-0");                
+        tile.classList.add("winner");
+        gameOver = true;
+        return;
+    }
+}
