5a232fd1c093b34f1399c8c406304ff1cbefb313
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-28 14:55:45 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,31 +1,152 @@
-let move = 0;
-
-registerOnclick((x, y) => {
-  const marker = move % 2 === 0 ? 'X' : 'O';
-  const color = move % 2 === 0 ? 'red' : 'blue';
-
-  const row = 1 + Math.floor(y / (height / 3));
-  const col = Math.floor(x / (width / 3));
+const readline = require("readline");
 
 
-  const xPos = (col * width / 3) + width / 9;
-  const yPos = (row * height / 3) - (height / 9) + (height / 18);
-
-
-  drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
-
-  move++;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+let player = 'X';
+let ticktacktoe = [
+    ' ', ' ', ' ',
+    ' ', ' ', ' ',
+    ' ', ' ', ' '
+];
 
-const board = () => {
-  for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * width / 3
-    drawLine(x, 0, x, height, 'black', 5);
-  }
-  for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * height / 3
-    drawLine(0, x, width, x, 'black', 5);
-  }
+const defaultPlayer = () => {
+    rl.question("Default player is X, Do you want to change it, y or n ", function (defaultOrChoose) {
+        let choice = defaultOrChoose.toLowerCase();
+        if (choice == 'y') {
+            setPlayer();
+        } else if (choice === 'n') {
+            console.log(`Starting player is X`);
+            printBoard();
+            playTickTacToe();
+
+        } else {
+            defaultPlayer();
+        }
+
+
+    });
+
 }
 
-board()
+const setPlayer = () => {
+
+    rl.question("Default player is X, Do you want to change it, enter O? ", function (whichPlayer) {
+
+        changePlayer(whichPlayer);
+    });
+
+}
+
+const changePlayer = (playerChange) => {
+
+    let mov = playerChange.toUpperCase();
+
+    if (mov === 'O' || mov === 'X') {
+        console.log(`Starting player is ${mov}`)
+        player = mov;
+        printBoard();
+        playTickTacToe();
+    } else {
+        setPlayer();
+    }
+
+}
+
+const playTickTacToe = () => {
+    let continePlay = false;
+    for (i = 0; i < ticktacktoe.length; i++) {
+        if (ticktacktoe[i] === ' ') {
+            continePlay = true;
+        }
+    }
+
+    if (continePlay === true) {
+        rl.question("Play Game ? ", function (movve) {
+            gameplayer(movve)
+        });
+
+    }
+};
+
+const gameplayer = (move) => {
+
+    let moveToCheck = parseInt(move) - 1;
+    let i;
+    for (i = 0; i < ticktacktoe.length; i++) {
+
+        if (moveToCheck == i && ticktacktoe[i] === ' ') {
+
+            ticktacktoe[i] = player;
+            if (player === 'X') {
+                player = 'O';
+            } else if (player === 'O') {
+                player = 'X'
+            }
+
+
+        } else {
+            playTickTacToe();
+        }
+    }
+
+    console.log('\n');
+    printBoard();
+    const winner = calculateWinner();
+    if (winner != ' ') {
+        console.log(`Winner is ${winner}`)
+        process.exit(0);
+    }
+    playTickTacToe();
+
+}
+
+const calculateWinner = () => {
+
+    if (ticktacktoe[0] == ticktacktoe[1] && ticktacktoe[0] == ticktacktoe[2]) {
+        return ticktacktoe[0];
+
+    } else if (ticktacktoe[3] == ticktacktoe[4] && ticktacktoe[3] == ticktacktoe[5]) {
+        return ticktacktoe[3];
+
+    } else if (ticktacktoe[6] == ticktacktoe[7] && ticktacktoe[6] == ticktacktoe[8]) {
+        return ticktacktoe[6];
+
+    } else if (ticktacktoe[0] == ticktacktoe[3] && ticktacktoe[0] == ticktacktoe[6]) {
+        return ticktacktoe[0];
+
+    } else if (ticktacktoe[1] == ticktacktoe[4] && ticktacktoe[1] == ticktacktoe[7]) {
+        return ticktacktoe[1];
+
+    } else if (ticktacktoe[2] == ticktacktoe[5] && ticktacktoe[2] == ticktacktoe[8]) {
+        return winner = ticktacktoe[2];
+
+    } else if (ticktacktoe[0] == ticktacktoe[4] && ticktacktoe[0] == ticktacktoe[8]) {
+        return ticktacktoe[0];
+    } else if (ticktacktoe[2] == ticktacktoe[4] && ticktacktoe[2] == ticktacktoe[6]) {
+        return ticktacktoe[2];
+    }
+
+    return ' ';
+}
+
+
+const printBoard = () => {
+
+    let line = "";
+    for (let i = 1; i < 10; i++) {
+        line += ticktacktoe[i - 1] + ' | ';
+
+        if (i % 3 === 0) {
+            console.log(line);
+            console.log('____________')
+            line = "";
+        }
+
+    }
+}
+
+printBoard();
+defaultPlayer();
