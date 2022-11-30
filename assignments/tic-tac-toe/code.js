88e9c3039b11af77f4baf03e01abee0f13729b53
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-11-30 15:21:03 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -6,21 +6,6 @@ const board = [
   ['', '', ''],
 ];
 
-
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [1, 4, 7],
-  [0, 3, 6],
-];
-
-
-
 registerOnclick((x, y) => {
   const marker = move % 2 === 0 ? 'X' : 'O';
   const color = move % 2 === 0 ? 'red' : 'blue';
