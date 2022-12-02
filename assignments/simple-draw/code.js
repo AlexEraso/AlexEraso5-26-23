8137eb232e2a6740c613eec1d81a1335efb63f2a
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-12-02 15:24:19 -0800

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -6,10 +6,13 @@
 
 //vertical
 
+drawLine(-800, -200, 900, 300, 'red', 30);
+
+
 //diagonal left to right 
 
 //drawLine(-800, -200, 900, 300, 'red', 30);
 
 //diagonal right to left
 
-drawLine(0, 400, 900, 0,'red', 30);
+//drawLine(0, 400, 900, 0,'red', 30);
