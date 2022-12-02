a15886dfa4e78057828f5202556d9739db47eba7
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-12-02 14:44:48 -0800

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,6 +1,42 @@
+/*
+ * This code is running in an environment with five functions defined:
+ *
+ *  drawLine(x1, y1, x2, y2, color, lineWidth)
+ *
+ *    Draws a line from x1,y1 to x2,y2 using the give color. The fifth argument,
+ *    lineWidth, is optional and defaults to 1.
+ *
+ *  drawCircle(x, y, r, color, lineWidth=1)
+ *
+ *    Draws a circle centered at x,y with radius r using the given color. The
+ *    fith argument, lineWidth, is optional and defaults to 1.
+ *
+ *  drawRect(x, y, width, height, color, lineWidth=1)
+ *
+ *    Draws a rectangle starting at x,y with the given width, height, and color.
+ *    Positive widths go to the right and negative to the left; positive heights
+ *    go down and negative heights go up. The sixth argument, lineWidth, is
+ *    optional and defaults to 1.
+ *
+ *  drawFilledRect(x, y, width, height, color)
+ *
+ *    Draws a filled rectangle starting at x,y with the given width, height, and
+ *    color. Positive widths go to the right and negative to the left; positive
+ *    heights go down and negative heights go up.
+ *
+ *  drawFilledCircle(x, y, r, color)
+ *
+ *    Draws a filled circle centered at x,y with radius r using the given color.
+ *
+ * There are also a couple variables that might be useful:
+ *
+ *  width - the width of the drawing area.
+ *  height - the height of the drawing area.
+ *
+ */
 
- //*  drawLine(x1, y1, x2, y2, color, lineWidth)
 
 
+//drawLine(x1, y1, x2, y2, color, lineWidth)
 
-drawLine(-200, height - 200, 900, 300, 'green' 500);
+drawLine(-10, height - 200, 900, 300, 'green');
