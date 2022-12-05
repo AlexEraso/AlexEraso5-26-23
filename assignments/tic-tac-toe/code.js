d07be68016e0a1254d6bde89e45ca0c71a770235
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-12-05 15:31:23 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -29,23 +29,23 @@ function checkWinner() {
   //checking horizontally
   for (let r = 0; r < 3; r++) {
     if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != '') {
-      drawLine(0, 100, 1000, 100, 'yellow', 30);
+      drawLine(0, 80, 1000, 80, 'yellow', 20);
     }
   }
   //checking vertically
   for (let c = 0; c < 3; c++) {
     if (board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[0][c] != '') {
-      drawLine(0, 100, 1000, 100, 'yellow', 30);
+      drawLine(0, 100, 1000, 100, 'yellow', 20);
     }
   }
   //checking diagonally from left to right
   if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '') {
-    drawLine(-800, - 200, 900, 300, 'yellow', 30);
+    drawLine(-800, - 200, 900, 300, 'yellow', 20);
   }
 
   //checking diagonally from right to left
   if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '') {
-    drawLine(0, 400, 900, 0, 'yellow', 30);
+    drawLine(0, 400, 900, 0, 'yellow', 20);
   }
 }
 
