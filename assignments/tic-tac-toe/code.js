75b44a10b2ecfb59bb00fa18d6625d1019e4a402
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-12-08 15:13:37 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -10,7 +10,7 @@ registerOnclick((x, y) => {
   const marker = move % 2 === 0 ? 'X' : 'O';
   const color = move % 2 === 0 ? 'red' : 'blue';
 
-  const row = Math.floor(y / (height / 3));
+  const row = 1 + Math.floor(y / (height / 3));
   const col = Math.floor(x / (width / 3));
 
   const xPos =  (col * width / 3) + width / 9;
