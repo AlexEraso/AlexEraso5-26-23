246d6820abf4b69e72c218319d095944470c3ac4
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-12-08 15:19:22 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -16,6 +16,8 @@ registerOnclick((x, y) => {
   const xPos =  (col * width / 3) + width / 9;
   const yPos = (row * height / 3) - (height / 9) + (height / 18);
 
+  const floor = Math.floor(y/(h/3));
+
   if (board[row - 1][col] === '') {
     drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
 
@@ -29,13 +31,13 @@ function checkWinner() {
   //checking horizontally: Change the 0 and 1000 to a variable 
   for (let r = 0; r < 3; r++) {
     if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != '') {
-      drawLine(0, 60, 1000, 60, 'yellow', 20);
+      drawLine(0, (floor * height/3) + height/6, 1000, (floor * height/3) + height/6, 'yellow', 20);
     }
   }
   //checking vertically: Change 400 and 0 to a variable
   for (let c = 0; c < 3; c++) {
     if (board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[0][c] != '') {
-      drawLine(750, 0, 750, 400, 'yellow', 20);
+      drawLine((floor * width/3) + width/6, 0, (floor * width/3) + width/6, 400, 'yellow', 20);
 
     }
   }
