cc782d78594c9c476bdce6f3218d3c6cd882567d
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-12-08 20:54:44 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -49,30 +49,6 @@ function checkWinner() {
   }
 }
 
-
-
-
-
-//drawLine(x1, y1, x2, y2, color, lineWidth)
-
-//horizontal
-
-//drawLine(0, 100, 1000, 100, 'red', 30);
-
-//vertical
-
-//drawLine(100, 400, 100, 0, 'red', 30);
-
-
-//diagonal left to right 
-
-//drawLine(-800, -200, 900, 300, 'red', 30);
-
-//diagonal right to left
-
-//drawLine(0, 400, 900, 0,'red', 30);
-
-
 const drawBoard = () => {
   for (let i = 0; i < 2; i++) {
     const x = (i + 1) * width / 3
