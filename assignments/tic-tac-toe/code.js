58e4b61e1b250178a613b2849bcc272ca36a0238
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2022-12-20 16:29:07 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -29,23 +29,23 @@ function checkWinner() {
   //checking horizontally
   for (let r = 0; r < 3; r++) {
     if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != '') {
-      drawLine(0, (height / 6) + (r * height / 3), width, (height / 6) + (r * height / 3), "yellow", 20);
+      drawLine(0, (height / 6) + (r * height / 3), width, (height / 6) + (r * height / 3), "yellow", 10);
     }
   }
   //checking vertically
   for (let c = 0; c < 3; c++) {
     if (board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[0][c] != '') {
-      drawLine((width / 6) + (c * width / 3) - (width/100), 0, (width / 6) + (c * width / 3)- (width/100), height, "yellow", 20);
+      drawLine((width / 6) + (c * width / 3) - (width/100), 0, (width / 6) + (c * width / 3)- (width/100), height, "yellow", 10);
     }
   }
   //checking diagonally from left to right
   if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '') {
-    drawLine(0, 0, width, height, 'yellow', 20);
+    drawLine(0, 0, width, height, 'yellow', 10);
   }
 
   //checking diagonally from right to left
   if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '') {
-    drawLine(0, height, width, 0, 'yellow', 20);
+    drawLine(0, height, width, 0, 'yellow', 10);
   }
 }
 const drawBoard = () => {
