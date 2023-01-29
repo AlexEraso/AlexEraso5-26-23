5154a9151ae53ca0f998ae5c85478d7d9af05b1c
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-01-28 21:56:48 -0800

    Revert "Changing the Mouse Follower"

--- projects/web-demos/public/follow-mouse/script.js
+++ projects/web-demos/public/follow-mouse/script.js
@@ -1,14 +1,14 @@
-const dog = document.getElementById('orangutan');
+const dog = document.getElementById('dog');
 
 // Move the mouse DIV around to follow the mouse pointer.
-document.documentElement.onmousemove = (e) => {
-  orangutan.style.left = `${e.x}px`;
-  orangutan.style.top = `${e.y}px`;
+document.documentElement.ondogmove = (e) => {
+  dog.style.left = `${e.x}px`;
+  dog.style.top = `${e.y}px`;
 };
 
 document.documentElement.onclick = (e) => {
   // Make a copy of the original mouse div.
-  const m = orangutan.cloneNode(true);
+  const m = dog.cloneNode(true);
 
   // Remove the id attribute so when we add it the document it doesn't conflict
   // with the original.
