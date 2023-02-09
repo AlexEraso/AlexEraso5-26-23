09d37fe207a488eec6eb20e23584b9dc05850b84
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-02-09 23:16:51 +0000

    Broken timer

--- projects/pomodoro-timer/public/scripts/script.js
+++ projects/pomodoro-timer/public/scripts/script.js
@@ -1,4 +1,4 @@
-<script src="/scripts/script.js" type="module"></script>
+
 
 let nIntervalId;
 
@@ -7,19 +7,32 @@ function startTimer() {
 };
 
 function stopTimer() {
-clearInterval(nIntervalId)
-nIntervalId = null;
-};
-
-function fiveMins() {
 
 };
 
-function twentyMins() {
+function fiveminutetimer(){
+    var sec = 300;
+    var timer = setInterval(function(){
+        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
 
-};
+function twentyfivetimer(){
+    var sec = 1500;
+    var timer = setInterval(function(){
+        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
 
-function setInterval () {
+function count () {
     setInterval(myTickFunc, 1000);
     elapsedMs += 1000;
     if (elapsedMs >= 5000) {doReverse = true;}
@@ -30,13 +43,4 @@ document.getElementById("stop").addEventListener("click", );
 
 p.querySelector('#counter').textContent = `${counter}`;
 
-function timer(){
-    var sec = 30;
-    var timer = setInterval(function(){
-        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
-}
+
