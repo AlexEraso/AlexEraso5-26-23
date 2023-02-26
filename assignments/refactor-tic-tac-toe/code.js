91497ef15338774354ed805558ac2db754e280e3
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-02-25 22:15:55 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -109,8 +109,8 @@ registerOnclick((x, y) => {
   // Only do anything if it's a legal move and the game isn't over.
   if (winner === null && 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '') {
 
-makeMove(r, c);
-
+    makeMove(r, c);
+    //EXTRACTING ARGUMENTS is sorta just making it a top level function, then doing the const thing, putting the local variables in the arguments, and calling it again with the locals arguments at the bottom, very repetitive process.
     // Check if there's a winner now
     winner = findWinner();
     if (winner !== null) {
