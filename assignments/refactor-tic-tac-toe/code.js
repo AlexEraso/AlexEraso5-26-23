5b93e580a009d1b6aea60dd867f3378b81010fa0
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-02-26 19:17:22 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -1,12 +1,28 @@
-const snowBall = (x, y, radius) => {
-  drawCircle(x, y, radius + 2, 'black', 3);
-  drawFilledCircle(x, y, radius, 'white', 3);
-};
+const drawPicture = (horizon, base, size) => {
 
-const drawHead = (x, headY, headSize) => {
-  const headRadius = headSize / 2;
+  // Draw the background
+  drawFilledRect(0, 0, width, horizon, '#ddeeff');
+  drawFilledRect(0, horizon, width, height, 'white');
+  drawLine(0, horizon, width, horizon, '#bbb');
+
+  // Draw the snowman
+  const x = width / 2;
+  const proportions = [3, 4, 5];
+  const [headP, torsoP, buttP] = proportions;
+  const total = proportions.reduce((tot, p) => tot + p, 0);
+
+  const headSize = size * (headP / total);
+  const torsoSize = size * (torsoP / total)
+  const buttSize = size * (buttP / total);
+
+  const headY = (base - size) + headSize / 2;
+  const torsoY = headY + headSize / 2 + torsoSize / 2;
+  const buttY = torsoY + torsoSize / 2 + buttSize / 2;
+
   // Draw the head
-  snowBall(x, headY, headRadius);
+  const headRadius = headSize / 2;
+  drawCircle(x, headY, headRadius + 2, 'black', 3);
+  drawFilledCircle(x, headY, headRadius, 'white', 3);
 
   // Draw the eyes
   const eyeSpacing = headRadius * 0.25;
@@ -31,68 +47,29 @@ const drawHead = (x, headY, headSize) => {
   const hatHeight = headRadius * 1.25;
   drawFilledRect(x - brimWidth / 2, brimTop, brimWidth, brimHeight, 'black');
   drawFilledRect(x - hatWidth / 2, brimTop - hatHeight, hatWidth, hatHeight, 'black');
-};
 
-const drawArm = (x, y, radius, direction) => {
-  let x1 = x + radius * 0.6 * direction;
-  let x2 = x + radius * 2.35 * direction;
-  /*
-  // why doesn't this work?
-  let x1 = x + radius * 0.6;
-  let x2 = x + radius * 2.35;
-  if (isLeftArm) {
-    x1 = x1 * -1;
-    x2 = x2 * -1;
-  }
-  */
-  drawLine(x1, y - radius * 0.25, x2, y - radius * 0.85, 'black', 3);
-};
-
-const drawTorso = (x, torsoY, torsoSize) => {
-  const torsoRadius = torsoSize / 2;
   // Draw the torso
-  snowBall(x, torsoY, torsoRadius);
+  const torsoRadius = torsoSize / 2;
+  drawCircle(x, torsoY, torsoRadius + 2, 'black', 3);
+  drawFilledCircle(x, torsoY, torsoRadius, 'white', 3);
 
   // Draw the arms
-  drawArm(x, torsoY, torsoRadius, 1);
-  drawArm(x, torsoY, torsoRadius, -1);
-  
+  let x1 = x + torsoRadius * 0.6;
+  let x2 = x + torsoRadius * 2.35;
+  drawLine(x1, torsoY - torsoRadius * 0.25, x2, torsoY - torsoRadius * 0.85, 'black', 3);
+  x1 = x + torsoRadius * 0.6 * -1;
+  x2 = x + torsoRadius * 2.35 * -1;
+  drawLine(x1, torsoY - torsoRadius * 0.25, x2, torsoY - torsoRadius * 0.85, 'black', 3);
+
   // Draw the buttons
   for (let i = 0; i < 3; i++) {
     drawFilledCircle(x, torsoY - torsoRadius * 0.5 + i * torsoRadius * 0.5, 4, 'black');
   }
 
-};
-
-const drawPicture = (horizon, base, size) => {
-
-  // Draw the background
-  drawFilledRect(0, 0, width, horizon, '#ddeeff');
-  drawFilledRect(0, horizon, width, height, 'white');
-  drawLine(0, horizon, width, horizon, '#bbb');
-
-  // Draw the snowman
-  const x = width / 2;
-  const proportions = [3, 4, 5];
-  const [headP, torsoP, buttP] = proportions;
-  const total = proportions.reduce((tot, p) => tot + p, 0);
-
-  const headSize = size * (headP / total);
-  const torsoSize = size * (torsoP / total)
-  const buttSize = size * (buttP / total);
-
-  const headY = (base - size) + headSize / 2;
-  const torsoY = headY + headSize / 2 + torsoSize / 2;
-  const buttY = torsoY + torsoSize / 2 + buttSize / 2;
-
-  // Draw the head
-  drawHead(x, headY, headSize);
-
-  // Draw the torso
-  drawTorso(x, torsoY, torsoSize);
-
   // Draw the butt
-  snowBall(x, buttY, buttSize / 2);
+  const buttRadius = buttSize / 2;
+  drawCircle(x, buttY, buttRadius + 2, 'black', 3);
+  drawFilledCircle(x, buttY, buttRadius, 'white', 3);
 };
 
 drawPicture(height * 0.7, height * 0.9, height * 0.7);
