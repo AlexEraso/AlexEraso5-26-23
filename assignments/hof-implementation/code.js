59ebfbdde43381452a26b46b2baae09fb62b6720
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-02-28 14:50:25 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -1,9 +1,12 @@
 // N.B. Do not use the array methods of the same name to implement these functions!
 
 const filter = (predicate, array) => {
-let array = []
+let nArray = []
 for (let i = 0; i < array.length; i++)
-
+if (predicate (array[x])) {
+  nArray.push(array[x])
+}
+return nArray
 };
 
 const map = (fn, array) => {
