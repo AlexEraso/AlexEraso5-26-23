8d4dc069205e43cc3f77008dc84d59cec82790fe
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-02-28 14:58:13 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -11,7 +11,7 @@ const filter = (predicate, array) => {
 
 const map = (fn, array) => {
   let nArray = []
-  for (let i = 0; i < array.length; x++) {
+  for (let i = 0; i < array.length; i++) {
   nArray.push(fn(array[i]))
 }
 return nArray
