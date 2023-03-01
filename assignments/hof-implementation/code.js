e7d8322da5dd4577a653ce443a03600cc5414ed7
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-03-01 14:42:46 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -12,24 +12,27 @@ const filter = (predicate, array) => {
 const map = (fn, array) => {
   let nArray = []
   for (let i = 0; i < array.length; i++) {
-  nArray.push(fn(array[i]))
-}
-return nArray
+    nArray.push(fn(array[i]))
+  }
+  return nArray
 };
 
 const flatMap = (fn, array) => {
-let nArray = []
+  let nArray = []
   for (let i = 0; i < array.length; i++) {
-
+    let iArray = fn(array.length[i])
+    for (let i = 0; i < array.length; i++) {
+      nArray.push(iArray[i])
+    }
   }
   return nArray
 };
 
 const reduce = (fn, initialValue, array) => {
- for (let i = 0; i < array.length; i++) {
- initialValue = fn(initialValue, array[i])
- }
- return initialValue
+  for (let i = 0; i < array.length; i++) {
+    initialValue = fn(initialValue, array[i])
+  }
+  return initialValue
 };
 
 const every = (predicate, array) => {
