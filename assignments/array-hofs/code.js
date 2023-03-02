1b76e146da4a0268bdba01ed932de62a7876e97c
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-03-02 15:15:11 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -22,8 +22,8 @@ const pairs = (array) => {
   return array.map((n) => [n, n])
 };
 
-const averageGrade = () => {
-
+const averageGrade = (array) => {
+return array.map((n) => n.grade)
 };
 
 const flatpairs = (array) => {
@@ -38,6 +38,6 @@ const someEven = (array) => {
 return array.some((n) => n % 2 ===0)
 };
 
-const lengthOfNames = () => {
-
+const lengthOfNames = (array) => {
+return array.filter
 };
