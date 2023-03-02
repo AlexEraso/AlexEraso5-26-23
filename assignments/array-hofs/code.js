0f7b5b9a38e9a49b82b47955dae89c4adb793c77
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-03-02 15:10:40 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -1,25 +1,25 @@
 const evens = (array) => {
-return array.filter((n) => n % 2 === 0)
+  return array.filter((n) => n % 2 === 0)
 };
 
 const odds = (array) => {
-return array.filter((n) => n % 2 !== 0)
+  return array.filter((n) => n % 2 !== 0)
 };
 
 const big = (array) => {
-return array.filter ((n) => n > 100)
+  return array.filter((n) => n > 100)
 };
 
 const names = (array) => {
-return array.map((n) => n.name)
+  return array.map((n) => n.name)
 };
 
 const grades = (array) => {
-return array.map((n) => n.grade)
+  return array.map((n) => n.grade)
 };
 
 const pairs = (array) => {
-return array.map ((n) => [n ,n])
+  return array.map((n) => [n, n])
 };
 
 const averageGrade = () => {
@@ -27,11 +27,11 @@ const averageGrade = () => {
 };
 
 const flatpairs = (array) => {
-return array.flatMap((n) => [n ,n])
+  return array.flatMap((n) => [n, n])
 };
 
 const allEven = () => {
-
+return array.every((n) => n % 2 ===0)
 };
 
 const someEven = () => {
