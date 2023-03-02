a79e14c824eb327504ae51fdec26d2defbedc8ef
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-03-02 14:44:47 -0800

    Updating assignments/array-hofs/code.js

---
+++ assignments/array-hofs/code.js
@@ -0,0 +1,39 @@
+const evens = () => {
+
+};
+
+const odds = () => {
+
+};
+
+const big = () => {
+
+};
+
+const grades = () => {
+
+};
+
+const pairs = () => {
+
+};
+
+const averageGrade = () => {
+
+};
+
+const flatpairs = () => {
+
+};
+
+const allEven = () => {
+
+};
+
+const someEven = () => {
+
+};
+
+const lengthOfNames = () => {
+
+};
