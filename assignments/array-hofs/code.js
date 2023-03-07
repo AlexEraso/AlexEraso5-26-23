317ad4e652965a32b5a2420439e91a02c60c46ee
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-03-07 14:40:39 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -1,23 +1,43 @@
 const evens = (array) => {
   return array.filter((n) => n % 2 === 0)
-}
+};
 
 const odds = (array) => {
-  return array.filter ((n) => n % 2 !== 0)
-}
+  return array.filter((n) => n % 2 !== 0)
+};
 
 const big = (array) => {
-  return array.filter ((n) => n > 100)
-}
+  return array.filter((n) => n > 100)
+};
 
 const names = (array) => {
-  return array.map ((n) => n.name )
-}
+  return array.map((n) => n.name)
+};
 
 const grades = (array) => {
-  return array.map ((n) => n.grade)
-}
+  return array.map((n) => n.grade)
+};
 
 const pairs = (array) => {
-  return array.map ((n) => [n,n])
-}
+  return array.map((n) => [n, n])
+};
+
+const averageGrade = (array) => {
+return array.map((n) => n.grade).reduce((n, tot) => tot + n)/array.length
+};
+
+const flatpairs = (array) => {
+  return array.flatMap((n) => [n, n])
+};
+
+const allEven = (array) => {
+return array.every((n) => n % 2 ===0)
+};
+
+const someEven = (array) => {
+return array.some((n) => n % 2 ===0)
+};
+
+const lengthOfNames = (array) => {
+return array.filter((n) => n[0].toUpperCase() === n[0]).map((n) => n.length)
+};
