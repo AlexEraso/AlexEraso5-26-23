f32e9132722dc7fdc8f2b9750f4df4d15c4befa0
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-03-08 14:46:49 -0800

    Updating assessments/2023-03-08-hofs/code.js

--- assessments/2023-03-08-hofs/code.js
+++ assessments/2023-03-08-hofs/code.js
@@ -1,1 +1,13 @@
-const 
+const evens = (array) => {
+  let r = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      r.push(array[i]);
+    }
+  }
+  return r;
+};
+
+
+
+
