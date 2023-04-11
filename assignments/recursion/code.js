21e772638a06cfadfce410a307d2af8e6d92f545
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-04-11 15:20:15 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -15,9 +15,13 @@ const triangular = (n) => {
 }
 
 const fibonacci = (n) => {
-  if (n ===0) {
+  if (n === 0) {
     return 0;
   } else {
-    return fibonacci(n - 1) * fibonacci(n - 1);
+    if (n === 1) {
+      return 1;
+    } else {
+      return fibonacci(n - 1) * fibonacci(n - 1);
+    }
   }
 }
