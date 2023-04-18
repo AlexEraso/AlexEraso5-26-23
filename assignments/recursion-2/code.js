f8c4d4c001032c48692aee20d76884d47132f84a
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-04-18 15:07:29 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -5,3 +5,11 @@ const add = (a, b) => {
     return 1 + add(a, b - 1);
   }
 }
+
+const multiply = (a, b) => {
+  if (b === 0) {
+    return 0;
+  } else {
+    return a + multiply(a, b - 1)
+  }
+}
