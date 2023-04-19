7d8771e7d3ddb653651d4464ccfeba61d1a12ad9
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-04-19 14:49:30 -0700

    Creating assignments/recursion-3/code.js

---
+++ assignments/recursion-3/code.js
@@ -0,0 +1,31 @@
+const product = () => {
+
+}
+
+const sumSquares = () => {
+
+}
+
+const lucas = () => {
+
+}
+
+const isAscending = () => {
+
+}
+
+const isDescending = () => {
+
+}
+
+const sumNested = () => {
+
+}
+
+const searchNested = () => {
+
+}
+
+const evaluate = () => {
+
+}
