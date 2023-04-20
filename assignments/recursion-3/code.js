36675041adb5f75e93c0f5cc9642b5b3e4d3b77f
AlexEraso <111394851+AlexEraso@users.noreply.github.com>
2023-04-20 15:16:22 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -7,35 +7,38 @@ const sumSquares = () => {
 }
 
 const lucas = (n) => {
-  if (n === 0) {
-    return 2;
+  if (n === 1) {
+    return 1
   } else {
-    return lucas(n - 1) + lucas(n - 2);
+    if (n === 0) {
+      return 2;
+    } else {
+      return lucas(n - 1) + lucas(n - 2);
+    }
   }
-}
 
-const isAscending = (numbers) => {
-  if (numbers.length < 2 ) {
-  return true
-  } else {
+  const isAscending = (numbers) => {
+    if (numbers.length < 2) {
+      return true
+    } else {
       return numbers[0] <= numbers[1] && isAscending(numbers.slice(1));
     }
   }
 
 
 
-const isDescending = () => {
+  const isDescending = () => {
 
-}
+  }
 
-const sumNested = () => {
+  const sumNested = () => {
 
-}
+  }
 
-const searchNested = () => {
+  const searchNested = () => {
 
-}
+  }
 
-const evaluate = () => {
+  const evaluate = () => {
 
-}
+  }
